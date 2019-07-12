6ff3d7c3b47fa0c86c8a14762696145cd7468334
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-07-13 00:57:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ let techKaro = {
     longDetail: "CIRCLE is a social enterprise for women empowerment which focuses on growing women leaders, startups and promoting women in tech.It has launched a project in Karachi’s Usman Institute of Technology with partnership of Engro Vopak Terminal Limited to provide training to the under- served community especially women.The idea is to utilize the potential of women to contribute to their families and economy.",
     sponser: "engro vopak",
     funding: "2,00,000",
-
-
+    lastBatch: "50 graduates",
+    events: ["eid millan party", "graduation cermony"],
+    courseOutline: ["html", "css", "java script", "wordpress", "react"],
+    html: "1 month",
+    css: "2 months",
+    javaScript: "2 months",
+    reactWordPress: "3 months",
+    techNinjas: 9,
+    certificates: [
+        {
+            name: "freecodecamp",
+            course: ["responsive web design", "java script", "react", "wordpress"]
+        },
+        {
+            name: "tech karo",
+            course: "web development"
+        },
+        {
+            name: "uit",
+            course: "web designing"
+        },
+    ],
     staff: [
         {
             ownerName: "Sadaffe Abid",
@@ -178,34 +198,124 @@ let techKaro = {
                 salary: "25,000"
             },
             {
-                name: "Kiran",
-                education: "graduation continue",
-                dob: "30-5-1999",
-                institute: "federa university",
-                sibilings: "2 sisters and 3 brother",
+            name: "Kiran",
+            education: "graduation continue",
+            dob: "30-5-1999",
+            institute: "federa university",
+            sibilings: "2 sisters and 3 brother",
+            status: "single",
+            attendance: "90%",
+            address: "block-16, house number: 28, Gulistan-e-johar",
+            convience: "moterbike",
+            hasInternet: "true",
+            hasComputer: "true",
+            cnic: "42000-54796986-3",
+            bornPlace: "Karachi",
+            schoolName: "rose grammar School",
+            employment: {
+                isJob: "true",
+                salary: "15,000",
+                distanceInKm: "20",
+                jobTitle: "Computer operator",
+                jobTiming: "8:00-6:00",
+                officeName: "Export company"
+            },
+            guardianDetail: {
+                name: "Sultan Khan",
+                relation: "father",
+                destination: "private employ",
+                salary: "50,000"
+            },
+            {
+                name: "hamza",
+                education: "matric",
+                dob: "13-8-2001",
+                institute: "the city",
                 status: "single",
-                attendance: "90%",
-                address: "block-16, house number: 28, Gulistan-e-johar",
+                attendance: "95%",
+                address: "block-C, house number: 130, samama",
+                convience: "moter bike",
+                hasInternet: "true",
+                hasComputer: "true",
+                cnic: "42000-245752873-4",
+                bornPlace: "Karachi",
+                schoolName: "the city",
+                sibilings: "1 sister and 2 brother",
+                employment: {
+                    isJob: "falsee",
+                    salary: "0",
+                    distanceInKm: "05",
+                    jobTitle: "none",
+                    jobTiming: "none",
+                    officeName: "none"
+                },
+                guardianDetail: {
+                    name: "Waqas",
+                    relation: "father",
+                    destination: "private employ",
+                    salary: "38,000",
+                }
+            },
+            {
+                name: "ALiya",
+                education: "masters",
+                dob: "10-12-1992",
+                institute: "university of karachi",
+                status: "single",
+                attendance: "95%",
+                address: "block-14, house number: 300, shahra e faisal",
+                convience: "public-transport",
+                hasInternet: "true",
+                hasComputer: "true",
+                cnic: "42000-1986752-4",
+                bornPlace: "Karachi",
+                schoolName: "everst grammar School",
+                sibilings: "1 sister and 0 brother",
+                employment: {
+                    isJob: "false",
+                    salary: "0",
+                    distanceInKm: "50",
+                    jobTitle: "none",
+                    jobTiming: "none",
+                    officeName: "none"
+                },
+                guardianDetail: {
+                    name: "Ahmed",
+                    relation: "father",
+                    destination: "pilot",
+                    salary: "50,000",
+                }
+            },
+            {
+                name: "Salar",
+                education: "masters",
+                dob: "30-7-1991",
+                institute: "university of karachi",
+                status: "married",
+                attendance: "75%",
+                address: "block-19, house number: 12, gulsahn-e-iqbal",
                 convience: "moterbike",
                 hasInternet: "true",
                 hasComputer: "true",
-                cnic: "42000-54796986-3",
+                cnic: "42000-3834798-4",
                 bornPlace: "Karachi",
-                schoolName: "rose grammar School",
+                schoolName: "paf school",
+                sibilings: "1 sister and 2 brother",
                 employment: {
                     isJob: "true",
-                    salary: "15,000",
+                    salary: "10,000",
                     distanceInKm: "20",
-                    jobTitle: "Computer operator",
-                    jobTiming: "8:00-6:00",
-                    officeName: "Export company"
+                    jobTitle: "teacher",
+                    jobTiming: "7:30-1:30",
+                    officeName: "Beacon Light"
                 },
                 guardianDetail: {
-                    name: "Imran Khan",
+                    name: "Mehmood",
                     relation: "father",
                     destination: "private employ",
-                    salary: "25,000"
-                },
+                    salary: "30,000",
+                }
+            },
         },
     ]
 }
