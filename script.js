90a7a3cbe358ebfcc36dff750876f70c8661769f
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-07-12 21:40:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let techKaro = {
+    name: "Tech Karo",
+    course: "web development",
+    duration: "8 months",
+    shortDetail: "tech karo is the institute where student learn how to code",
+    longDetail: "CIRCLE is a social enterprise for women empowerment which focuses on growing women leaders, startups and promoting women in tech.It has launched a project in Karachi’s Usman Institute of Technology with partnership of Engro Vopak Terminal Limited to provide training to the under- served community especially women.The idea is to utilize the potential of women to contribute to their families and economy.",
+    students: [
+        {
+            name: "Urooj",
+            education: "masters",
+            dob: "1-7-1992",
+            institute: "university of karachi",
+            employment: {
+                isJob: "true",
+                salary: "10,000",
+                distanceInKm: "20",
+                jobTitle: "teacher",
+                jobTiming: "7:30-1:30",
+                officeName: "Beacon Light"
+            },
+            sibilings: "1 sister and 2 brother",
+            guardianDetail: {
+                name: "Ali",
+
+            }
+        }
+    ]
+}
